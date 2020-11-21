0676e4fc36fd2c0ed8bbf9c2633707e67f58e0ff
Haibin Zhu <ssspiderzzz@gmail.com>
2020-11-21 14:09:00 -0800

    update new flight status checking api, update new fields for frontend codes

--- src/frontcomponents/ScheduleListTable.js
+++ src/frontcomponents/ScheduleListTable.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './ScheduleListTable.css'
 
-export default function ScheduleListTable (props) {
+export default function ScheduleListTable(props) {
   // const now = new Date();
   // const y = now.getYear() + 1900;
   // const m = now.getMonth() + 1;
   // const d = now.getDate();
 
-  function beautyTime (time) {
+  function beautyTime(time) {
     let ampm = ''
     let temp = time.split('T')[1]
     temp.split(':')
@@ -29,7 +29,6 @@ export default function ScheduleListTable (props) {
         <div id='scheduleRoot'>
           <table className='box-table'>
             <thead id='tableHead'>
-              {/* <tr>{`Flight Schedule from ${props.newDeparture.name} to ${props.newArrival[0].name} on ${y}/${m}/${d}`}</tr> */}
               <tr>
                 <th scope='col'>Flight</th>
                 <th scope='col'>Airline</th>
@@ -39,22 +38,22 @@ export default function ScheduleListTable (props) {
             </thead>
             <tbody>
               {!props.newSchedule.error &&
-                props.newSchedule.scheduledFlights.map((schedule, index) => {
+                props.newSchedule.data.map((schedule, index) => {
                   return (
                     <tr key={index}>
                       <td>
-                        {schedule.carrierFsCode} {schedule.flightNumber}
+                        {schedule.flight.iata}
                       </td>
                       <td>
                         <img
                           id='airlinelogo'
                           alt='airlinelogo'
                           style={{ opacity: 1, maxHeight: 30, maxWidth: 50 }}
-                          src={`http://pics.avs.io/500/250/${schedule.carrierFsCode}.png`}
+                          src={`http://pics.avs.io/500/250/${schedule.airline.iata}.png`}
                         ></img>
                       </td>
-                      <td>{beautyTime(schedule.departureTime)}</td>
-                      <td>{beautyTime(schedule.arrivalTime)}</td>
+                      <td>{beautyTime(schedule.departure.scheduled)}</td>
+                      <td>{beautyTime(schedule.arrival.scheduled)}</td>
                     </tr>
                   )
                 })}
