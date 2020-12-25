4682b5de7f2c8eba59f0caa9fe9880ca2f4be8e1
Haibin Zhu <ssspiderzzz@gmail.com>
2020-12-24 23:21:27 -0800

    add new redux reducer to handle loading items numbers and status

---
+++ src/store/reducer.js
@@ -0,0 +1,23 @@
+export const TOTAL_ITEMS = 'TOTAL_ITEMS'
+export const LOADING_ITEMS_UPDATE = 'LOADING_ITEMS_UPDATE'
+export const LOADING_STATUS_UPDATE = 'LOADING_STATUS_UPDATE'
+
+const initState = {
+  totalItems: 0,
+  itemsLoaded: 0,
+  loadingCompleted: false,
+}
+
+export const reducer = (state = initState, action) => {
+  switch (action.type) {
+    case TOTAL_ITEMS:
+      return { ...state, totalItems: action.totalItems }
+    case LOADING_ITEMS_UPDATE:
+      return { ...state, itemsLoaded: action.itemsLoaded }
+    case LOADING_STATUS_UPDATE:
+      return { ...state, loadingCompleted: true }
+
+    default:
+      return state
+  }
+}
