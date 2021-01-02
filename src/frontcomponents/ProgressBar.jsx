2896f5fca085881443a5d3ef55f2ef33159acddb
Haibin Zhu <ssspiderzzz@gmail.com>
2021-01-02 11:44:54 -0800

    make loading component display full screen

--- src/frontcomponents/ProgressBar.jsx
+++ src/frontcomponents/ProgressBar.jsx
@@ -9,7 +9,7 @@ export default function ProgressBar(props) {
   const loadingPercentage = ((loadingStatus.itemsLoaded / loadingStatus.itemsTotal) * 100).toFixed()
   const loadingOpacity = loadingStatus.loadingCompleted ? 0.5 : 1
   return (
-      <div style={{width: window.innerWidth, height: window.innerHeight, opacity:loadingOpacity}}className='loading'> 
+      <div style={{opacity:loadingOpacity}}className='loading'> 
         <p className='loadingLineOne'>Welcome to <span className='loadingSkylinks'>Skylinks</span></p>
         <p className='loadingLineTwo'>Thank you for your patience</p>
         <p className='loadingLineThree'>We are preparing for take-off</p>
