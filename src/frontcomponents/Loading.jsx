b01bd143fcb228694d4586d470a937b9c0ff7315
Haibin Zhu <ssspiderzzz@gmail.com>
2021-01-06 19:37:25 -0800

    add button to launch the app after the loading is completed

--- src/frontcomponents/Loading.jsx
+++ src/frontcomponents/Loading.jsx
@@ -1,30 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import loadingGif from "../assets/loading.svg"
 import "./Loading.css";
 import { useSelector } from 'react-redux'
 import SkylinksLogo from "../assets/SkylinksLogo.png";
 
 export default function Loading(props) {
+  const [launch, setLaunch] = useState(false)
   const loadingStatus = useSelector(state => state)
   const loadingPercentage = ((loadingStatus.itemsLoaded / loadingStatus.itemsTotal) * 100).toFixed()
-  const loadingOpacity = loadingPercentage === 100 ? 0 : 1
+  const loadingOpacity = launch ? 0 : 1
   return (
     <React.Fragment>
-      {!loadingStatus.loadingCompleted && 
         <div style={{opacity:loadingOpacity}}className='loading'> 
           <p className='loadingLineOne'>Welcome to 
             <span className='loadingSkylinks'>Skylinks</span>
           </p>
           <p className='loadingLineTwo'>Thank you for your patience</p>
           <p className='loadingLineThree'>We are preparing for take-off</p>
-          <img className ='loadingGif' src={loadingGif} alt="loadingGif"/>
-          <div className='outterBar'>
-            <div className='innerBar' style={{ width: `${loadingPercentage}%` }}>
-              <img className="loadingLogo" src={SkylinksLogo} alt="plane" />
-            </div>
-          </div>
+
+          {!loadingStatus.loadingCompleted &&
+            <>
+              <img className ='loadingGif' src={loadingGif} alt="loadingGif"/>
+              <div className='outterBar'>
+                <div className='innerBar' style={{ width: `${loadingPercentage}%` }}>
+                  <img className="loadingLogo" src={SkylinksLogo} alt="plane" />
+                </div>
+              </div>
+            </>
+          }
+          {loadingStatus.loadingCompleted && 
+            <button onClick={() => {setLaunch(true)}}> Take off now! </button>
+          }
         </div>
-      }
+      
     </React.Fragment>
   );
 }
