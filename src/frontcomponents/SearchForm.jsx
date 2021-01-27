dab5dbba6c6173a5cfa34fa8896808521abeaac7
Haibin Zhu <ssspiderzzz@gmail.com>
2021-01-26 17:10:06 -0800

    add city and airport name search for search form, add fuse.js for fuzzy search, remove 3d airplane model from client

--- src/frontcomponents/SearchForm.jsx
+++ src/frontcomponents/SearchForm.jsx
@@ -1,22 +1,67 @@
 import React, { useState } from "react";
+import Fuse from "fuse.js";
+import airportFullNames from "../assets/airportFullNames.json";
 import "./SearchForm.css";
+
+const options = {
+  shouldSort: true,
+  includeScore: true,
+  distance: 300,
+  threshold: 0.2,
+  keys: ["airportName"],
+};
 
 const SearchForm = (props) => {
   const [inputState, setInputState] = useState("");
+  const [fsCodeState, setFsCodeState] = useState("");
+  let airportNames;
+  if (inputState) {
+    const fuseName = new Fuse(airportFullNames, options);
+    airportNames = fuseName.search(inputState).slice(0, 10);
+  }
 
   return (
     <div className="searchFormContainer">
       <span className="searchInputButtonWrapper">
         <span className="searchInputWrapper">
           <input
-            onChange={(event) => setInputState(event.target.value)}
+            value={inputState}
+            onChange={(event) => {
+              setInputState(event.target.value);
+              setFsCodeState(event.target.value);
+            }}
             placeholder='Try "YVR"'
           ></input>
         </span>
         <span className="searchButtonWrapper">
-          <button onClick={() => props.getDepartures(inputState)}>Search</button>
+          <button
+            onClick={() => {
+              props.getDepartures(fsCodeState);
+            }}
+          >
+            Search
+          </button>
         </span>
       </span>
+      <div id="myDropdown" class="dropdown-content">
+        {airportNames ? (
+          airportNames.map((airport) => {
+            return (
+              <span
+                key={`${airport.item.fs}`}
+                onClick={() => {
+                  setInputState(airport.item.airportName);
+                  setFsCodeState(airport.item.fs);
+                }}
+              >
+                {airport.item.airportName}
+              </span>
+            );
+          })
+        ) : (
+          <span key={`No Results`}>No Matches Found</span>
+        )}
+      </div>
     </div>
   );
 };
